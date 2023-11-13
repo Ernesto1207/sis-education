a9a6e2131569f872a6bf2c0cddadfe3ccb891245
Ernesto1207 <ernestosolanoballena@hotmail.com>
2023-11-13 08:01:06 -0500

    graphics

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -144,12 +144,12 @@
 
     <div class="grid grid-cols-2 gap-20 pt-20">
         <div class="shadow-lg rounded-lg overflow-hidden">
-            <div class="py-3 px-5 bg-gray-900 text-white">Bar chart</div>
+            <div class="py-3 px-5 bg-gray-900 text-white">Grafico de Usuarios</div>
             <canvas class="" id="chartBar"></canvas>
         </div>
 
         <div class="shadow-lg rounded-lg overflow-hidden">
-            <div class="py-3 px-5 bg-gray-900 text-white">Pie chart</div>
+            <div class="py-3 px-5 bg-gray-900 text-white">Grafico de Asistencias</div>
             <canvas class="ml-20 mr-40" id="chartPie"></canvas>
         </div>
     </div>
