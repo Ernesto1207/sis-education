eb37f67d24212d66d578cebc65c305bf2630e22c
Ernesto1207 <ernestosolanoballena@hotmail.com>
2023-11-22 22:58:57 -0500

    dark mode

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,7 @@
 
     <!-- Styles -->
     @livewireStyles
+
 </head>
 
 <body class="font-sans antialiased">
@@ -75,8 +76,7 @@
                                             <!-- start::Submenu content -->
                                             <div class="bg-white rounded">
                                                 <!-- start::Submenu link -->
-                                                <a x-data="{ linkHover: false }"
-                                                    href="{{ route('perfil.mostrar') }}"
+                                                <a x-data="{ linkHover: false }" href="{{ route('perfil.mostrar') }}"
                                                     class="flex items-center justify-between py-2 px-3 hover:bg-gray-100 bg-opacity-20"
                                                     @mouseover="linkHover = true" @mouseleave="linkHover = false">
                                                     <div class="flex items-center">
@@ -118,6 +118,34 @@
                                                         </div>
                                                     </div>
                                                 </a>
+                                                <!-- end::Submenu link -->
+
+                                                <!-- start::Submenu link -->
+                                                <button id="theme-toggle" type="button"
+                                                    class="flex items-center justify-between py-2 px-3 hover:bg-gray-100 bg-opacity-20"
+                                                    @mouseover="linkHover = true" @mouseleave="linkHover = false">
+                                                    <div class="flex items-center">
+                                                        <svg id="theme-toggle-dark-icon" class="hidden w-5 h-5"
+                                                            fill="currentColor" viewBox="0 0 20 20"
+                                                            xmlns="http://www.w3.org/2000/svg">
+                                                            <path
+                                                                d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z">
+                                                            </path>
+                                                        </svg>
+                                                        <svg id="theme-toggle-light-icon" class="hidden w-5 h-5"
+                                                            fill="currentColor" viewBox="0 0 20 20"
+                                                            xmlns="http://www.w3.org/2000/svg">
+                                                            <path
+                                                                d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                                                                fill-rule="evenodd" clip-rule="evenodd"></path>
+                                                        </svg>
+                                                        <div class="text-sm ml-3">
+                                                            <p class="text-gray-600 font-bold capitalize"
+                                                                :class="linkHover ? 'text-primary' : ''">Dark Mode</p>
+                                                        </div>
+                                                    </div>
+                                                </button>
+                                               
                                                 <!-- end::Submenu link -->
 
                                                 <hr>
@@ -272,7 +300,49 @@
     </div>
 
     @stack('modals')
+    <script>
+        var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+        var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 
+        // Change the icons inside the button based on previous settings
+        if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia(
+                '(prefers-color-scheme: dark)').matches)) {
+            themeToggleLightIcon.classList.remove('hidden');
+        } else {
+            themeToggleDarkIcon.classList.remove('hidden');
+        }
+
+        var themeToggleBtn = document.getElementById('theme-toggle');
+
+        themeToggleBtn.addEventListener('click', function() {
+
+            // toggle icons inside button
+            themeToggleDarkIcon.classList.toggle('hidden');
+            themeToggleLightIcon.classList.toggle('hidden');
+
+            // if set via local storage previously
+            if (localStorage.getItem('color-theme')) {
+                if (localStorage.getItem('color-theme') === 'light') {
+                    document.documentElement.classList.add('dark');
+                    localStorage.setItem('color-theme', 'dark');
+                } else {
+                    document.documentElement.classList.remove('dark');
+                    localStorage.setItem('color-theme', 'light');
+                }
+
+                // if NOT set via local storage previously
+            } else {
+                if (document.documentElement.classList.contains('dark')) {
+                    document.documentElement.classList.remove('dark');
+                    localStorage.setItem('color-theme', 'light');
+                } else {
+                    document.documentElement.classList.add('dark');
+                    localStorage.setItem('color-theme', 'dark');
+                }
+            }
+
+        });
+    </script>
     @livewireScripts
 </body>
 
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -253,7 +253,7 @@
     class="fixed z-30 inset-y-0 left-0 w-64 transition duration-300 bg-secondary overflow-y-auto lg:translate-x-0 lg:inset-0 custom-scrollbar">
     <!-- start::Logo -->
     <div class="flex items-center justify-center bg-black bg-opacity-30 h-16">
-        <h1 class="text-gray-100 text-lg font-bold uppercase tracking-widest">
+        <h1 class="text-gray-900 text-lg font-bold uppercase tracking-widest">
             Template
         </h1>
     </div>
@@ -267,13 +267,13 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
             </svg>
-            <span class="ml-3 transition duration-200" :class="linkHover ? 'text-gray-100' : ''">
+            <span class="ml-3 transition duration-200" :class="linkHover ? 'text-gray-100' : 'text-gray-900'">
                 Dashboard
             </span>
         </a>
         <!-- end::Menu link -->
 
-        <p class="text-xs text-gray-600 mt-10 mb-2 px-6 uppercase">Usuarios</p>
+        <p class="text-xs text-black mt-10 mb-2 px-6 uppercase">Usuarios</p>
 
         <!-- start::Menu link roles -->
         @if (auth()->user()->can('administrador'))
@@ -518,89 +518,89 @@
                         </a>
                     </li>
                     <!-- end::Submenu link -->
-                </ul>
-                <!-- end::Submenu -->
-            </div>
-        @endcan
-        <!-- end::Menu link -->
+            </ul>
+            <!-- end::Submenu -->
+        </div>
+    @endcan
+    <!-- end::Menu link -->
 
-        <!-- start::Menu link Partes -->
-        @if (auth()->user()->can('administrador') ||
-                auth()->user()->can('profesor') ||
-                Auth::user()->can('alumno'))
-            <div x-data="{ linkHover: false, linkActive: false }">
-                <div @mouseover="linkHover = true" @mouseleave="linkHover = false"
-                    @click="linkActive = !linkActive"
-                    class="flex items-center justify-between text-gray-400 hover:text-gray-100 px-6 py-3 cursor-pointer hover:bg-black hover:bg-opacity-30 transition duration-200"
-                    :class="linkActive ? 'bg-black bg-opacity-30 text-gray-100' : ''">
-                    <div class="flex items-center">
-                        <span class="ml-3">Partes de Conducta</span>
-                    </div>
-                    <svg class="w-3 h-3 transition duration-300"
-                        :class="linkActive ? 'rotate-90' : ''" fill="none"
-                        stroke="currentColor" viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M9 5l7 7-7 7">
-                        </path>
-                    </svg>
+    <!-- start::Menu link Partes -->
+    @if (auth()->user()->can('administrador') ||
+            auth()->user()->can('profesor') ||
+            Auth::user()->can('alumno'))
+        <div x-data="{ linkHover: false, linkActive: false }">
+            <div @mouseover="linkHover = true" @mouseleave="linkHover = false"
+                @click="linkActive = !linkActive"
+                class="flex items-center justify-between text-gray-400 hover:text-gray-100 px-6 py-3 cursor-pointer hover:bg-black hover:bg-opacity-30 transition duration-200"
+                :class="linkActive ? 'bg-black bg-opacity-30 text-gray-100' : ''">
+                <div class="flex items-center">
+                    <span class="ml-3">Partes de Conducta</span>
                 </div>
-                <!-- start::Submenu -->
-                <ul x-show="linkActive" x-cloak x-collapse.duration.300ms
-                    class="text-gray-400">
-                    <!-- start::Submenu link -->
-                    <li
-                        class="pl-10 pr-6 py-2 cursor-pointer hover:bg-black hover:bg-opacity-30 transition duration-200 hover:text-gray-100">
-                        <a href="{{ route('conducta.index') }}" class="flex items-center">
-                            <span class="mr-2 text-sm">&bull;</span>
-                            <span class="overflow-ellipsis">Partes de Conducta</span>
-                        </a>
-                    </li>
-                    <!-- end::Submenu link -->
-                </ul>
-                <!-- end::Submenu -->
+                <svg class="w-3 h-3 transition duration-300"
+                    :class="linkActive ? 'rotate-90' : ''" fill="none"
+                    stroke="currentColor" viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M9 5l7 7-7 7">
+                    </path>
+                </svg>
             </div>
-        @endcan
-        <!-- end::Menu link -->
+            <!-- start::Submenu -->
+            <ul x-show="linkActive" x-cloak x-collapse.duration.300ms
+                class="text-gray-400">
+                <!-- start::Submenu link -->
+                <li
+                    class="pl-10 pr-6 py-2 cursor-pointer hover:bg-black hover:bg-opacity-30 transition duration-200 hover:text-gray-100">
+                    <a href="{{ route('conducta.index') }}" class="flex items-center">
+                        <span class="mr-2 text-sm">&bull;</span>
+                        <span class="overflow-ellipsis">Partes de Conducta</span>
+                    </a>
+                </li>
+                <!-- end::Submenu link -->
+            </ul>
+            <!-- end::Submenu -->
+        </div>
+    @endcan
+    <!-- end::Menu link -->
 
-        <!-- start::Menu link Justificaciones -->
-        @if (auth()->user()->can('administrador') ||
-                auth()->user()->can('profesor') ||
-                Auth::user()->can('alumno'))
-            <div x-data="{ linkHover: false, linkActive: false }">
-                <div @mouseover="linkHover = true" @mouseleave="linkHover = false"
-                    @click="linkActive = !linkActive"
-                    class="flex items-center justify-between text-gray-400 hover:text-gray-100 px-6 py-3 cursor-pointer hover:bg-black hover:bg-opacity-30 transition duration-200"
-                    :class="linkActive ? 'bg-black bg-opacity-30 text-gray-100' : ''">
-                    <div class="flex items-center">
-                        <span class="ml-3">Justificaciones</span>
-                    </div>
-                    <svg class="w-3 h-3 transition duration-300"
-                        :class="linkActive ? 'rotate-90' : ''" fill="none"
-                        stroke="currentColor" viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            stroke-width="2" d="M9 5l7 7-7 7">
-                        </path>
-                    </svg>
+    <!-- start::Menu link Justificaciones -->
+    @if (auth()->user()->can('administrador') ||
+            auth()->user()->can('profesor') ||
+            Auth::user()->can('alumno'))
+        <div x-data="{ linkHover: false, linkActive: false }">
+            <div @mouseover="linkHover = true" @mouseleave="linkHover = false"
+                @click="linkActive = !linkActive"
+                class="flex items-center justify-between text-gray-400 hover:text-gray-100 px-6 py-3 cursor-pointer hover:bg-black hover:bg-opacity-30 transition duration-200"
+                :class="linkActive ? 'bg-black bg-opacity-30 text-gray-100' : ''">
+                <div class="flex items-center">
+                    <span class="ml-3">Justificaciones</span>
                 </div>
-                <!-- start::Submenu -->
-                <ul x-show="linkActive" x-cloak x-collapse.duration.300ms
-                    class="text-gray-400">
-                    <!-- start::Submenu link -->
-                    <li
-                        class="pl-10 pr-6 py-2 cursor-pointer hover:bg-black hover:bg-opacity-30 transition duration-200 hover:text-gray-100">
-                        <a href="{{ route('justificaciones.index') }}"
-                            class="flex items-center">
-                            <span class="mr-2 text-sm">&bull;</span>
-                            <span class="overflow-ellipsis">Justificaciones</span>
-                        </a>
-                    </li>
-                    <!-- end::Submenu link -->
-                </ul>
-                <!-- end::Submenu -->
+                <svg class="w-3 h-3 transition duration-300"
+                    :class="linkActive ? 'rotate-90' : ''" fill="none"
+                    stroke="currentColor" viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        stroke-width="2" d="M9 5l7 7-7 7">
+                    </path>
+                </svg>
             </div>
-        @endcan
-        <!-- end::Menu link -->
-    </nav>
+            <!-- start::Submenu -->
+            <ul x-show="linkActive" x-cloak x-collapse.duration.300ms
+                class="text-gray-400">
+                <!-- start::Submenu link -->
+                <li
+                    class="pl-10 pr-6 py-2 cursor-pointer hover:bg-black hover:bg-opacity-30 transition duration-200 hover:text-gray-100">
+                    <a href="{{ route('justificaciones.index') }}"
+                        class="flex items-center">
+                        <span class="mr-2 text-sm">&bull;</span>
+                        <span class="overflow-ellipsis">Justificaciones</span>
+                    </a>
+                </li>
+                <!-- end::Submenu link -->
+            </ul>
+            <!-- end::Submenu -->
+        </div>
+    @endcan
+    <!-- end::Menu link -->
+</nav>
 </aside>
